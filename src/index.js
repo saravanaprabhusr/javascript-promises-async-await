8fa05c80222fb5dc5170600b1ee1aed0fa70a66f
Saravanaprabhu <saravanaprabhu.sr@salesforce.com>
2021-09-23 16:07:56 +0530

    Completed module

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-export function fetchMovies(){
+import { fetchWithTimeout } from './services';
 
+export function fetchMovies(){
+    let movies = require('./data/movies.json')
+
+    const resolveFunction = () => movies;
+    return fetchWithTimeout(1000)
+        .then(resolveFunction);
 }
+
+let moviePromise = fetchMovies();
+moviePromise.then( (results) => {
+    console.log(results);
+});
