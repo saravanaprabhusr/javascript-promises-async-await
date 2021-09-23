946c6da0a740d0c58163e950b696bc27ad60963d
Saravanaprabhu <saravanaprabhu.sr@salesforce.com>
2021-09-23 16:28:20 +0530

    Completed module

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
+import { result } from 'lodash';
 import { fetchWithTimeout } from './services';
-const movies = require('./data/movies.json')
-export function fetchMovies(){
-    const resolveFunction = () => movies;
-    return fetchWithTimeout(1000)
-        .then(resolveFunction);
+import { fetchBooks } from './services';
+import { fetchMovies } from './services';
+
+export function getBooksAndMovies(){
+    return Promise.all([fetchBooks(), fetchMovies()])
+        .then(([books, movies ]) => ({
+            books,
+            movies
+        }))
+        .catch(error => console.log("Error fetching books and movies", error));
 }
 
-let moviePromise = fetchMovies();
-moviePromise.then( (results) => {
-    console.log(results);
+const getBooksAndMoviesPromise = getBooksAndMovies();
+getBooksAndMoviesPromise.then( results => {
+    console.log('getBooksAndMoviesPromise', results);
 });
+
+export function getBooksOrMovies(){
+    return Promise.race([fetchBooks(), fetchMovies()])
+        .then(([books, movies ]) => ({
+            books,
+            movies
+        }))
+        .catch(error => console.log("Error fetching books and movies", error));
+}
+
+const getBooksOrMoviesPromise = getBooksOrMovies();
+getBooksOrMoviesPromise.then( results => {
+    console.log('getBooksOrMoviesPromise', result);
+})
